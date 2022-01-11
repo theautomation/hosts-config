372e25c600d89c6f764d925ff4544083cf0bb459
theautomation <github@theautomation.nl>
2022-01-11 19:44:49 +0100

    changed some text

--- proxmox-ve/docker-host/backup_synchronization.sh
+++ proxmox-ve/docker-host/backup_synchronization.sh
@@ -1,71 +1,70 @@
 #!/bin/bash
 #
-# this script will backup files and folders from local machine to a remote machine over ssh.
-# script writtin by Coen Stam.
+# This script will backup files and folders from local machine to a remote machine over ssh.
+# Writtin by Coen Stam.
 # github@theautomation.nl
 #
 
 set -ex
 
-# ----------------------------------------------------------------------------------- 
-# select if ssh configuation from /.ssh/config must be used or not. 
-# see also https://github.com/theautomation/hosts-config/tree/main/backup-host#setup-ssh
+# -----------------------------------------------------------------------------------
+# Select if ssh configuation from /.ssh/config must be used or not.
+# See also https://github.com/theautomation/hosts-config/tree/main/backup-host#setup-ssh
 # 1 = use the user, host and port from a ssh config file stored in /.ssh/config.
-# 0 = use ssh variables user, host and port from the variables below in this script.
-ssh_config="1"
+# 0 = use ssh variables user, host and port from the variables below.
+ssh_config="0"
 
-# if ssh_config => 1 
-# remote hostname from .ssh/config.
+# If ssh_config => 1
+# Remote hostname from .ssh/config.
 remote_host_from_config="backup-host"
 
-# if ssh_config => 0
-# user of the remote host.
+# If ssh_config => 0
+# User of the remote host.
 remote_user="coen"
-# ip address of the remote host.
-remote_host="192.168.1.234"
-# remote ssh port.
+# Ip address of the remote host.
+remote_host="192.168.30.127"
+# Remote ssh port.
 remote_port=2244
 
-# rsync options in command (optional).
-# more options can be found at https://linux.die.net/man/1/rsync
+# Rsync options in command (optional).
+# More options can be found at https://linux.die.net/man/1/rsync
 rsync_options="--archive --partial --stats --verbose --delete"
 
-# absolute path of the exclude list location for rsync as external file.
+# Absolute path of the exclude list location for rsync as external file.
 rsync_excludelist="/home/coen/hosts-config/proxmox-ve/docker-host/backup_exclude"
 
-# rsync source path being copied
+# Rsync source path being copied
 rsync_sourcepath="/mnt/slow-storage/coen
                   /mnt/slow-storage/anne"
 
-# destination path for rsync
+# Destination path for rsync
 rsync_destinationpath="/backup-pool/coen/rsync"
 # -----------------------------------------------------------------------------------
 
 echo "Starting backup of ${rsync_sourcepath}."
 
-
 if [ "${ssh_config}" = "1" ]; then
-  if [ ! -f ${HOME}/.ssh/config ]; then
-    echo "script stopped, ssh_config is set to 1 but there is no .ssh config file found in ${HOME}/.ssh/, create ssh config file or set ssh_config => 0 to use <user>, <host>, <port> from the variables in this script."
+  if [ ! -f "${HOME}"/.ssh/config ]; then
+    echo "Script stopped, ssh_config is set to 1 but there is no .ssh config file found in ${HOME}/.ssh/, create ssh config file or set ssh_config => 0 to use <user>, <host>, <port> from the variables in this script."
     exit 2
   elif [ -z "${remote_host_from_config}" ]; then
-    echo "script stopped, ssh_config is set to 1 but backup_host variable is empty."
+    echo "Script stopped, ssh_config is set to 1 but backup_host variable is empty."
     exit 3
   else
-  
-  echo "starting rsync using ssh config file"
-  rsync ${rsync_options} --exclude-from="${rsync_excludelist}" ${rsync_sourcepath} ${remote_host_from_config}:${rsync_destinationpath}
+
+    echo "Starting rsync using ssh config file"
+    rsync "${rsync_options}" --exclude-from="${rsync_excludelist}" "${rsync_sourcepath}" ${remote_host_from_config}:${rsync_destinationpath}
   fi
-    echo "start rsync with <user>, <host>, <port> from .ssh/config..."
+  echo "Start rsync with <user>, <host>, <port> from .ssh/config..."
 elif [ "${ssh_config}" = "0" ]; then
   if [ -z "${remote_user}" ] || [ -z "${remote_host}" ] || [ -z "${remote_port}" ]; then
-  echo "script stopped, ssh_config is set to 0 but one or more variables for are undefined, check remote user, host and port variable(s)"
-  exit 4
+    echo "script stopped, ssh_config is set to 0 but one or more variables for are undefined, check remote user, host and port variable(s)"
+    exit 4
   else
-  echo "start rsync with ${remote_user}, ${remote_host}, ${remote_port} from script variable..."
-  rsync ${rsync_options} --exclude-from="${rsync_excludelist}" --delete-excluded -e "ssh -p ${remote_port}" ${rsync_sourcepath} ${remote_user}@${remote_host}:${rsync_destinationpath}
+    echo "Start rsync with ${remote_user}, ${remote_host}, ${remote_port} from script variable..."
+    rsync "${rsync_options}" --exclude-from="${rsync_excludelist}" --delete-excluded -e "ssh -p ${remote_port}" "${rsync_sourcepath}" ${remote_user}@${remote_host}:${rsync_destinationpath}
   fi
 fi
 
-echo "all done..."
+echo "All done..."
 exit 0
